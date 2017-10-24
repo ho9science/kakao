b29c6faecc2e61f66f7eec042f7203b8e88c3998
samdasu <full17@naver.com>
2017-10-24 23:32:21 +0900

    version kkma

--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Kknouns(models.Model):
+	class Meta:
+		db_table='kknouns'
+	id=models.IntegerField(primary_key=True, max_length=11)
+	indexing=models.CharField(max_length=200)
+	answer=models.CharField(max_length=767)
--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -9,6 +9,9 @@ from rest_framework import status
 from quickstart.MyApp import MessageClass, KeyboardClass
 from rest_framework.parsers import JSONParser
 
+from konlpy.tag import Kkma
+from .models import Kknouns
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -44,11 +47,42 @@ class MessageView(APIView):
         get_arg1 = request.POST.get("arg1",None)
         get_arg2 = request.data
         
+        kkma = Kkma()
+
         print(get_arg2)
         get_arg1 = get_arg2.get('content')
-        print(get_arg2.get('content'))
+        if get_arg1=="welcome yapp":
+            myClass = MessageClass(get_arg1, *args, **kw)
+            result = myClass.do_work()
+            response = Response(result, status=status.HTTP_200_OK)
+            return response
+        get_arg1 = kkma.nouns(get_arg1)
+        kkma_list=get_arg1
+        return_value = ()
+        temp_id = ()
+        i = 0
+        answer = "no answer"
+        for keyword in kkma_list:
+            if i==0:
+                return_value = Kknouns.objects.values_list('id').filter(indexing__contains=keyword)
+            else:
+                return_value = Kknouns.objects.values_list('id').filter(id=temp_id,indexing__contains=keyword)
 
-        myClass = MessageClass(get_arg1, *args, **kw)
+            if return_value:
+                temp_id=return_value
+            i=i+1
+        print("kkma:{}".format(return_value))
+        if return_value:
+            print(type(return_value))
+            for gold in return_value:
+                int1 = gold[0]
+                print(type(int1))
+                answer = Kknouns.objects.values_list('answer').filter(id=int1)
+                break
+        
+        print(answer)
+                
+        myClass = MessageClass(answer, *args, **kw)
         result = myClass.do_work()
         response = Response(result, status=status.HTTP_200_OK)
         return response
